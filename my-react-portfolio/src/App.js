25fea5164e90378ca530ba16fb256ee2941f7b88
Jonelle Adona <jonelle.adona@knights.ucf.edu>
2021-02-13 10:39:38 -0500

    Add new files in components

--- my-react-portfolio/src/App.js
+++ my-react-portfolio/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import Header from "./components/Header";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import resumeData from "./resumeData";
+class App extends Component {
+  render() {
+    return (
+      <div className= "App">
+        <Header resumeData={resumeData}/>
+        <About resumeData={resumeData}/>
+        <Resume resumeData={resumeData}/>
+        <Projects resumeData={resumeData}/>
+        <Testimonials resumeData={resumeData}/>
+        <Contact resumeData={resumeData}/>
+        <Footer resumeData={resumeData}/>
+      </div>
+    );
+  }
 }
 
 export default App;
